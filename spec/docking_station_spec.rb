5adfc7c0b4cf2252f309a50e163128ec7074aa77
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2019-10-16 15:03:42 +0100

    Wk1(Day3): Add a test to DockingStation that verifies a bike is working.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,12 @@
 require './lib/docking_station.rb'
+require 'bike'
 
 describe DockingStation do
   it {is_expected.to respond_to :release_bike}
+
+  describe "#releases_bike" do
+    it('releases a working Bike') do
+      expect(subject.release_bike).to be_working
+    end
+  end
 end
