ccb53ac993fc013861a22f4babac10535f0a9da2
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2019-10-16 15:45:54 +0100

    Wk1(Day3): Passed instance of the bike to an instance of the DockingStation.

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,4 +4,12 @@ class DockingStation
   def release_bike
     Bike.new
   end
+
+  def dock(bike)
+    @bike = bike
+  end
+
+  def docked_bike
+    @bike
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,4 @@
-require './lib/docking_station.rb'
+require 'docking_station'
 require 'bike'
 
 describe DockingStation do
@@ -9,4 +9,18 @@ describe DockingStation do
       expect(subject.release_bike).to be_working
     end
   end
+
+  describe "#dock" do
+    bike = Bike.new
+    it'docks a bike.' do
+      expect(subject.dock(bike)).to eq bike
+    end
+  end
+
+  describe "#docked_bike" do
+    it "shows bike that has been docked." do
+        expect(subject.docked_bike).to eq @bike 
+    end
+  end
+
 end
